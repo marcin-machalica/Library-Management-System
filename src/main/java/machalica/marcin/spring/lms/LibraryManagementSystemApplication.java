feb7bc1cb008cb7c0eae11bf75e3afc7aa5b3ab5
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-04-07 11:39:07 +0200

    Initial commit

---
+++ src/main/java/machalica/marcin/spring/lms/LibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package machalica.marcin.spring.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+
+}
